f87db652588214467b80603a990ca0de54c9051f
Benjamin Crew <benncrew94@gmail.com>
2022-11-08 07:08:26 +0000

    Chapter 2 complete

--- second_practice/chapter2/clustered_linux_web_server/main.tf
+++ second_practice/chapter2/clustered_linux_web_server/main.tf
@@ -1,16 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.1.0"
     }
   }
 }
 
 provider "azurerm" {
-    tenant_id            = "3ccdf94d-2f74-44fb-a1f7-4814ebf776e7"
-    subscription_id      = "7c953d5a-e175-405e-bf11-a8cb0ecc9f09"
+  tenant_id       = "3ccdf94d-2f74-44fb-a1f7-4814ebf776e7"
+  subscription_id = "7c953d5a-e175-405e-bf11-a8cb0ecc9f09"
   features {}
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -29,5 +35,220 @@ resource "azurerm_subnet" "subnet" {
   name                 = "bc-uks-tst-tfupandrunning-sb01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["172.18.100.0/24"]
+  address_prefixes     = ["172.18.100.0/28"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "bc-uks-tst-tfupandrunning-pip-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "vmss_lb" {
+  name                = "bc-uks-tst-tfupandrunning-vmss-lb-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backendpool" {
+  loadbalancer_id = azurerm_lb.vmss_lb.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "vmss_probe" {
+  loadbalancer_id = azurerm_lb.vmss_lb.id
+  name            = "ssh-running-probe"
+  port            = var.server_port
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  loadbalancer_id                = azurerm_lb.vmss_lb.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = var.server_port
+  backend_port                   = var.server_port
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backendpool.id]
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.vmss_probe.id
+}
+
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "autoscale-config"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.vmss.id
+
+  profile {
+    name = "AutoScale"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 3
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.vmss.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.vmss.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 40
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "bc-uks-tst-tfupandrunning-nsg01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+
+resource "azurerm_network_security_rule" "ssh_rule" {
+  name                        = "ssh"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "httpd_rule" {
+  name                        = "httpd"
+  priority                    = 119
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = var.server_port
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# resource "azurerm_network_security_rule" "sql_region_rule" {
+#   name                       = "sql-region"
+#   priority                   = 118
+#   direction                  = "Outbound"
+#   access                     = "Allow"
+#   protocol                   = "Tcp"
+#   source_port_range          = "*"
+#   source_address_prefix      = "*"
+#   destination_port_range     = "11000-11999"
+#   destination_address_prefix = "Sql.UKSouth"
+#   resource_group_name         = azurerm_resource_group.rg.name
+#   network_security_group_name = azurerm_network_security_group.nsg.name
+# }
+
+# resource "azurerm_network_security_rule" "sql_gateway_rule" {
+#   for_each = local.sql_gateways
+
+#   name                       = each.value.rule_name
+#   priority                   = each.value.priority
+#   direction                  = "Outbound"
+#   access                     = "Allow"
+#   protocol                   = "Tcp"
+#   source_port_range          = "*"
+#   source_address_prefix      = "*"
+#   destination_port_range     = "1433"
+#   destination_address_prefix = each.value.dest_address
+#   resource_group_name         = azurerm_resource_group.rg.name
+#   network_security_group_name = azurerm_network_security_group.nsg.name
+# }
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                            = "bc-uks-tst-tfupandrunning-vmss-01"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  sku                             = "Standard_B1ls"
+  instances                       = 1
+  admin_username                  = "adminuser"
+  admin_password                  = "Xkmmdj3hgvy"
+  disable_password_authentication = false
+  custom_data                     = filebase64("run_apache.sh")
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  network_interface {
+    name                      = "vm_scale_set_interface"
+    primary                   = true
+    network_security_group_id = azurerm_network_security_group.nsg.id
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  lifecycle {
+    ignore_changes = [instances]
+  }
+}
+
+output "public_ip" {
+  description = "The public IP address of the web server"
+  value       = azurerm_public_ip.public_ip.ip_address
 }
